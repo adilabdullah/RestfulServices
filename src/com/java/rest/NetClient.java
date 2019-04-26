aaa8867e1aced3f6b1e01c063f6cbe7a241465c7
adilabdullah <adil_abdullah@hotmail.com>
2019-04-26 09:24:55 +0500

    first commit

---
+++ src/com/java/rest/NetClient.java
@@ -0,0 +1,48 @@
+package com.java.rest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetClient {
+	// http://localhost:8080/RESTfulExample/json/product/get
+		public static void main(String[] args) {
+
+		  try {
+
+			URL url = new URL("http://www.apilayer.net/api/live?access_key=c93f74c52b3c1094e1ab5e93b22f8fd4");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+
+		  } catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		  } catch (IOException e) {
+
+			e.printStackTrace();
+
+		  }
+
+		}
+}
